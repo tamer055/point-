22932eee767c076ec97a9c4b3eb7e7ee81de1868
temirlan <tima.omarbek@mail.ru>
2025-12-19 16:24:29 +0500

    test 1

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Ivan";
+        double salary = 55000.50;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
